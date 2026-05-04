c8bf77a30c042864f62b56c064e4f646e7b98688
caudrel <lozachaurelie@gmail.com>
2026-05-04 15:55:21 +0200

    deploy staging modified

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -3,14 +3,13 @@
 cd /root/apps/portfolio-app/staging/
 
 echo "Mise à jour du dépôt Git..."
-git fetch origin && git reset --hard origin/dev && git clean -f -d -e .env.staging && \
+git fetch origin && git reset --hard origin/dev && git clean -f -d -e .env.staging
 
-# Forcer l'arrêt des containers
-docker compose -f docker-compose.staging.yml --env-file .env.staging kill 2>/dev/null || true && \
-docker compose -f docker-compose.staging.yml --env-file .env.staging down --remove-orphans && \
+# Forcer la suppression des containers
+docker rm -f staging-gateway-1 staging-frontend-1 staging-redis-1 staging-backend-1 staging-db-1 2>/dev/null || true
 
 # Télécharger les dernières images
-docker compose -f docker-compose.staging.yml --env-file .env.staging pull && \
+docker compose -f docker-compose.staging.yml --env-file .env.staging pull
 
 # Relancer les services
 docker compose -f docker-compose.staging.yml --env-file .env.staging up -d
